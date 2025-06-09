0c3ad2c7d7531c9e22cb0742d6d9ef62863026c4
moaazreda55 <111019977+moaazreda55@users.noreply.github.com>
2025-06-09 19:28:03 +0300

    Create infer.py

---
+++ infer.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report , confusion_matrix ,accuracy_score
+import pickle
+
+df = pd.read_csv('data/500_Person.csv')
+
+df.dropna(inplace=True)
+
+df['Gender'] = LabelEncoder().fit_transform(df['Gender'])
+
+X = df.drop('Index',axis=1)
+y = df['Index']
+
+X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=45)
+
+model_tree = RandomForestClassifier()
+model_tree.fit(X_train,y_train)
+y_train_pred = model_tree.predict(X_train)
+y_pred = model_tree.predict(X_test)
+print(accuracy_score(y_train_pred,y_train))
+print(accuracy_score(y_pred,y_test))
+# print(classification_report(y_pred,y_test))
+# print(confusion_matrix(y_pred,y_test))
+
+
+with open('Weight Predection model_tree.pkl', 'wb') as file:
+    pickle.dump(model_tree, file)
