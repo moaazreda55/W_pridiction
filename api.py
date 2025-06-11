943151e7352ae95158eb4bdbb909b4a74ff2f757
moaazreda55 <111019977+moaazreda55@users.noreply.github.com>
2025-06-11 23:04:41 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,23 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-import pickle 
 import pandas as pd
+import onnxruntime as rt
+import numpy as np
 
 app = FastAPI()
 
 class Features(BaseModel):
-    features : List[float]  # List to hold all features (e.g., [Gender, Height, Weight])
-      
-with open("Weight Predection model_tree.pkl", "rb") as f:
-    model = pickle.load(f)    
-    
+    features: List[float]  # Example: [Gender, Height, Weight]
+
+# Load ONNX model
+session = rt.InferenceSession("model.onnx", providers=['CPUExecutionProvider'])
+
+# Get input name
+input_name = session.get_inputs()[0].name
+output_name = session.get_outputs()[0].name
+
 @app.get('/')
 def hello():
-    return {"message": "Hello from post or postman, FastAPI!"}
+    return {"message": "Hello from FastAPI with ONNX!"}
 
 @app.post('/predict/')
-async def predict(features:Features):
-    input_data = pd.DataFrame([features.features])
-    prediction = model.predict(input_data)
-    return {"prediction":int(prediction[0])}
+async def predict(features: Features):
+    input_data = np.array([features.features], dtype=np.float32)  # Must match training dtype
+    prediction = session.run([output_name], {input_name: input_data})
+    return {"prediction": int(prediction[0][0])}
